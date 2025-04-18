b2b9258cf437a84ed5b5ac8a6053623efd58da93
maiyalj <mlauralj7@gmail.com>
2025-04-17 21:12:26 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,15 +9,19 @@ int main(int argc, char *argv[]) {
         cerr << "Usage: " << argv[0] << " <test_file>" << endl;
         return 1;
     }
+    // open file 
     ifstream file(argv[1]);
     if (!file) {
         cerr << "Error: Unable to open file: " << argv[1] << endl;
         return 1;
     }
+    // set fixed number 
     const string fixedNum = "-123.456";
     string line;
+    // process lines in file 
     while (getline(file, line)) {
         cout << "Input: " << line << endl;
+        // declare valid or invalid 
         if (isValidDouble(line)) {
             string result = addDoubles(line, fixedNum);
             cout << "Double valid.\nResult: " << fixedNum << " = " << result << endl;
